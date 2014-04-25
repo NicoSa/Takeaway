5a48b1831d0189db46f46361910c873e1590446f
Nico Saueressig <nico@nicosaueressig.de>
2014-04-25 14:41:25 +0100

    added instructions for tasks in comments takeawayrb

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,4 +2,16 @@ class TakeAway
 
 end
 
-#change
+=begin
+	
+-list of dishes with prices
+-placing the order by giving the list of dishes, 
+- quantities,
+- exact total (raise an error if not correct)
+- customer is sent a text saying -> order placed successfully(delivered 1 hour from now, include current time), e.g. "Thank you! Your order was placed and will be delivered before 18:52".
+- use Twilio API
+- use mocks and/or stubs, as necessary to not to send texts when your tests are run
+- in IRB -> order is placed -> the text should actually be sent
+- Use your mobile phone number
+	
+=end
